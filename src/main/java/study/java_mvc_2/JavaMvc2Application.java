b7a86a13e93f2877bcf2d2235d90fd64e0fffc7c
NANOOS105 <heesu105@naver.com>
2025-01-02 22:34:11 +0900

    first commit

---
+++ src/main/java/study/java_mvc_2/JavaMvc2Application.java
@@ -0,0 +1,13 @@
+package study.java_mvc_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMvc2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMvc2Application.class, args);
+    }
+
+}
---
+++ src/test/java/study/java_mvc_2/JavaMvc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.java_mvc_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMvc2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
